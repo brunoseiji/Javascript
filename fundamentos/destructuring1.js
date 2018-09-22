0ce856ceba60403d5fdfdc40f15e5894796dfc75
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2018-09-21 23:04:50 -0300

    5 dia de curso

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,23 @@
+ // Novo recurso do ES2015
+
+ const pessoa = {
+     nome: 'Ana',
+     idade: 5,
+     endereco: {
+         logradouro: 'Rua ABC',
+         numero: 1000
+     }
+ }
+
+// É possível usar pessoa.nome, pessoa.sobrenome etc. Essa forma abaixo é um novo recurso que possui a mesma função.
+ const {nome, idade} = pessoa
+ console.log(nome, idade)
+
+ const {nome: n, idade: i} = pessoa
+console.log( n, i)
+
+const {sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
