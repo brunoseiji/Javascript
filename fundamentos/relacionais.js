90cee2bdc37dfb965a70d4e4d597410404c9f2c3
Bruno Seiji <bruno_seiji_1995@hotmail.com>
2018-09-21 23:41:07 -0300

    5 dia do curso

---
+++ fundamentos/relacionais.js
@@ -0,0 +1,25 @@
+/*
+* Com =, ele atribui um valor na variável
+* Com ==, ele compara 2 valores e retorna true ou false
+* Com ===, ele compara o tipo da variável (Se é string ou number)
+* Com != compara se o valor são diferentes
+* Com !== compara se os tipos das variáveis são diferentes e retornam true ou false
+*/
+
+console.log('01)', '1' == 1)
+console.log('02)', '1' === 1)
+console.log('03)', '3' != 3)
+console.log('04)', '3' !== 3)
+
+console.log('05)', 3 < 2)
+console.log('06)', 3 > 2)
+console.log('07)', 3 <= 2)
+console.log('08)', 3 >= 2)
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log('09)', d1 === d2)
+console.log('10)', d1 == d2)
+console.log('11)', d1.getTime() === d2.getTime())
+console.log('12)', undefined == null)
+console.log('13)', undefined === null)
